34032c6c6113120e91c8b19c175426b2cc4992d6
Oleksii Persatyi <persatyi@ukr.net>
2022-02-04 19:48:44 +0200

    Задание 7

---
+++ js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+                <a class="gallery__link" href="large-image.jpg">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </div>`;
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+gallery.addEventListener("click", openModal);
+
+let instance = null;
+
+function openModal(event) {
+  event.preventDefault();
+
+  if (event.target.tagName !== "IMG") {
+    return;
+  }
+
+  window.addEventListener("keydown", escCloseModal);
+
+  const dataSource = event.target.dataset.source;
+
+  instance = basicLightbox.create(`
+    <img src="${dataSource}" width="800" height="600">
+`);
+
+  instance.show();
+}
+
+function escCloseModal(event) {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
